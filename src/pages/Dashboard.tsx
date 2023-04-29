741ccd17471867070c25802d261adad1b9042583
Maxim Zakharov <maxeozakh@gmail.com>
2023-04-29 10:01:53 +0400

    chore: ignore json's with data for testing

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,9 @@
 import { Container } from '../entities/Container/Container'
 import { MetricsStats } from '../features/MetricsStats/MetricsStats'
+import { useRecordsStore } from '../shared/stores/records'
 
 export const Dashboard = () => {
+  const { addRecord } = useRecordsStore()
   return (
     <Container justifyContent="center">
       <MetricsStats />
