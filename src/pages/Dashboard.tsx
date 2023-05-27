7ce952924754ba65d95d789856a5ae86b686c261
Maxim Zakharov <maxeozakh@gmail.com>
2023-05-27 21:05:54 +0400

    ref: remove unnecessary dashboard state

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,30 +1,12 @@
-import { Text, View } from 'react-native'
-
 import { Container } from '../entities/Container/Container'
 import { MetricsStats } from '../features/MetricsStats/MetricsStats'
 // import { Testing } from '../features/Testing/Testing'
-import { getRecords } from '../shared/stores/records'
 
 export const Dashboard = () => {
-  const records = getRecords()
-  const isAnyRecords = records.length > 0
-
   return (
     <Container justifyContent="center">
       {/* <Testing /> */}
-      {isAnyRecords ? (
-        <MetricsStats />
-      ) : (
-        <View
-          style={{
-            alignItems: 'center',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-          }}>
-          <Text style={{ fontSize: 150 }}>🦊</Text>
-        </View>
-      )}
+      <MetricsStats />
     </Container>
   )
 }
